8d206e4e8e6833bc8dd3286e43ba044f2b423a5f
JMMG03 <lsrjk29@gmail.com>
2026-07-13 22:30:14 +0200

    añadir sitemap robots y pag 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://mariomolina.dev",
   adapter: cloudflare(),
+  integrations: [sitemap()],
 });
